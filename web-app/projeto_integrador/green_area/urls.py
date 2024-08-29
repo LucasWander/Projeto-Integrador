a7eb8f1240668a748f1fff40d06844e469bef47f
Lucas Wander <lucaswf48@gmail.com>
2024-08-29 19:37:10 -0300

    Adicionando mais rotas e atualizando o template

--- web-app/projeto_integrador/green_area/urls.py
+++ web-app/projeto_integrador/green_area/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.index, name="index")
+  path("", views.index, name="index"),
+  path("bairros", views.neighborhoods, name="bairros"),
+  path("bairros/<str:neighborhood_id>", views.neighborhoodDetail, name="detalhe-bairro")
 ]
--- web-app/projeto_integrador/green_area/views.py
+++ web-app/projeto_integrador/green_area/views.py
@@ -5,6 +5,29 @@ from django.template import loader
 def index(request):
   template = loader.get_template("green_area/index.html")
 
-  context = {}
+  bairros = [
+    {
+      "name": "Centro",
+      "id": "bairro-centro"
+    },
+    {
+      "name": "Chervezon",
+      "id": "bairro-chervezon"
+    },
+    {
+      "name": "Floridiana",
+      "id": "bairro-floridiana"
+    }
+  ]
+  context = {
+    "neighborhoods":bairros,
+    "name": "teste testando"
+  }
 
   return HttpResponse(template.render(context, request))
+
+def neighborhoods(request):
+  return HttpResponse("Bairros")
+
+def neighborhoodDetail(request, neighborhood_id):
+  return HttpResponse("Você está na página do bairro %s" % neighborhood_id)
